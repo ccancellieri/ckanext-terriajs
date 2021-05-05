333ab424b1fa4c8a02c95fd8f5dea70cf17ee6ec
ccancellieri <carlo.cancellieri@fao.org>
2021-05-05 22:29:26 +0200

    fixes the repo url in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/ccancellieri/ckanext-terriajs',
+    url='https://bitbucket.org/cioapps/ckanext-terriajs',
 
     # Author details
     author='''Carlo Cancellieri''',
