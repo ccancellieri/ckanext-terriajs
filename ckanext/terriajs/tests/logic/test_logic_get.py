87405a439d18b4a2bed615b07b0be7d9758d604f
Gianfranco Demarco <g.demarco@reply.it>
2021-12-06 19:10:10 +0100

    Refactored tests and introduced fixtures; added more tests; fixed default TYPE_MAPPING path

--- ckanext/terriajs/tests/logic/test_logic_get.py
+++ ckanext/terriajs/tests/logic/test_logic_get.py
@@ -1,14 +1,16 @@
 '''Tests for plugin.py.'''
+import json
+
 import ckan.plugins
-from ckan.plugins import toolkit
 import ckan.tests.factories as factories
 import ckan.tests.helpers as helpers
 import ckanext.terriajs.constants as constants
-import ckanext.terriajs.tools as tools
 import ckanext.terriajs.logic as getLogic
-from ckan.lib.helpers import get_site_protocol_and_host
 import ckanext.terriajs.logic.get as getLogic
-import json
+import pytest
+import six
+from ckan.plugins import toolkit
+from ckanext.terriajs.tests.conftest import _get_terriajs_view_params
 
 ckan_29_or_higher = toolkit.check_ckan_version(u'2.9')
 
@@ -27,146 +29,169 @@ class TestTerriaLogic(object):
     url_group_force_is_enabled = '''{host_url}{plugin_name}/terriajs_config/force_enabled/{resource_view_id}.json'''
     url_group = '''{host_url}{plugin_name}/terriajs_config/groups/{resource_view_id}'''
 
-    @classmethod
-    def setup_class(cls):
+
+    @pytest.fixture(autouse=True, scope="module")
+    def setup(cls):
+
         helpers.reset_db()
+
         # Test code should use CKAN's plugins.load() function to load plugins
         # to be tested.
         if not ckan.plugins.plugin_loaded('terriajs'):
             ckan.plugins.load('terriajs')
-        # Create user and context
-        cls.admin = factories.User()
-        cls.context = {
-            'ignore_auth': False,
-            'user': cls.admin['name']
-        }
-        cls.owner_org = factories.Organization(
-            users=[{'name': cls.admin['id'], 'capacity': 'admin'}]
-        )
-        cls.env = {'REMOTE_USER': cls.admin['name'].encode('ascii')}
+    
 
-        cls.package = factories.Dataset(owner_org=cls.owner_org['id'])
+    def test_we_can_enable_and_disable_an_item(self, resource):
 
-        cls.resource = factories.Resource(package_id=cls.package['id'], format='csv')
+        _resource_view_list = helpers.call_action('resource_view_list', id=resource['id'])
+        
+        # Check attribute not present as default
+        _item = _resource_view_list[0]
 
-        # Create view
-        # cls.package = factories.Dataset(owner_org=cls.owner_org['id'])
-        try:
-            resource_type = tools.map_resource_to_terriajs_type(cls.resource)
-        except Exception as e:
-            raise e
-        resource_type = 'csv'
+        assert 'isEnabled' not in _item
 
-        terriajs_schema = tools.get_schema(resource_type)
-        if not terriajs_schema:
-            raise Exception(resource_type + (' not defined, check your config'))
+        # Check enabled 
+        _item_after_logic = json.loads(getLogic.item_enabled(_item['id']))
+        assert 'isEnabled' in _item_after_logic
+        assert _item_after_logic['isEnabled'] == True
 
-        terriajs_config = tools.get_config(resource_type)
+        # Check disabled
+        _item_after_logic = json.loads(getLogic.item_disabled(_item['id']))
+        assert 'isEnabled' in _item_after_logic
+        assert _item_after_logic['isEnabled'] == False
 
-        cls.params = {
-            'resource_id': cls.resource['id'],
-            'view_type': constants.TYPE,
-            'description': 'A nice view',
-            constants.TERRIAJS_TYPE_KEY: resource_type,
-            constants.TERRIAJS_CONFIG_KEY: terriajs_config
-        }
 
-    def test_item_is_disabled_return_false(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _params = {
-            'package_id': _package['id'],
-            'url': 'http://data',
-            'name': 'A nice resource',
-            'format': 'csv'
-        }
-        _resource = helpers.call_action('resource_create', **_params)
+    def test_we_can_enable_and_disable_a_config(self, dataset):
+
+        # Create a Resource of type constants.LAZY_GROUP_TYPE and view of type constants.TYPE
+        _resource =  factories.Resource(package_id=dataset['id'], format=constants.LAZY_GROUP_TYPE)
+        factories.ResourceView(**_get_terriajs_view_params(_resource, resource_type=constants.LAZY_GROUP_TYPE))
+        
+        # Get the view just created using the list API and filtering on type
         _resource_view_list = helpers.call_action('resource_view_list', id=_resource['id'])
-        # Enable groups
-        isDisabled = getLogic.item_disabled(_resource_view_list[0]['id'])
-        data = json.loads(isDisabled)
-        assert (data['isEnabled'], False)
+        _group_resource_views = filter(lambda view: view[constants.TERRIAJS_TYPE_KEY] == constants.LAZY_GROUP_TYPE, _resource_view_list)
 
-    def test_item_is_enabled_return_true(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _params = {
-            'package_id': _package['id'],
-            'url': 'http://data',
-            'name': 'A nice resource',
-            'format': 'csv'
+        # Check at least a view of type constants.LAZY_GROUP_TYPE (ideally the one we just created) exist
+        assert len(_group_resource_views) > 0
+
+        # Get the view and set 2 items
+        _group_view = _group_resource_views[0]
+        _group_view[constants.TERRIAJS_CONFIG_KEY]['items'] = [
+            {
+                'id': _group_view['id'],
+                'type': constants.LAZY_ITEM_TYPE
+            },
+            {
+                'id': _group_view['id'],
+                'type': constants.LAZY_ITEM_TYPE
+            }
+        ]
+
+        # Update the view
+        _group_view = helpers.call_action('resource_view_update', **_group_view)
+        
+        
+        # The config_disabled goes in infite loop
+        # get.py .resolve calls itself recursively (line 136)
+
+        # Check if all items disabled
+        # config = json.loads(getLogic.config_disabled(_group_view['id']))
+        # assert len(filter(lambda catalog: catalog['isEnabled'] == True), config['catalog']) == 0
+
+        # Check if all items enabled
+        # config = json.loads(getLogic.config_enabled(_resource_view_list[0]['id']))
+        # assert len(filter(lambda catalog: catalog['isEnabled'] == False), config['catalog']) == 0
+
+        assert False
+
+
+    def test_jinja_interpolation_works_over_mandatory_fields(self, dataset):
+        """
+            We test that jinja correctly interpolates data from dataset/resource into the view
+            We use a simpler template than the actual one
+        """
+
+        resource = factories.Resource(package_id=dataset['id'], format="csv")
+        
+        view_body = _get_terriajs_view_params(resource)
+        view_body[constants.TERRIAJS_CONFIG_KEY] = {
+            'name': '{{dataset.title}}',
+            'url': '{{resource.url}}',
+            'description': '{{resource.description}}',
+            'id': '{{resource.id}}',
+            'type': 'csv'
         }
-        _resource = helpers.call_action('resource_create', **_params)
-        _resource_view_list = helpers.call_action('resource_view_list', id=_resource['id'])
-        # Enable groups
-        isEnabled = getLogic.item_enabled(_resource_view_list[0]['id'])
-        data = json.loads(isEnabled)
-        assert (data['isEnabled'], True)
 
-    def test_is_config_disabled(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _params = {
-            'package_id': _package['id'],
-            'url': 'http://data',
-            'name': 'A nice resource',
-            'format': 'csv'
+        _view = helpers.call_action('resource_view_create', context={}, **view_body)
+        assert _view['terriajs_type'] == view_body[constants.TERRIAJS_CONFIG_KEY]['type']
+
+        # We test actual interpolation
+        # We are expecting title as interpolated value
+        _item = json.loads(getLogic.item(_view['id']))
+
+        # View title should match the metadata title
+        assert _item['name'] == dataset['title']
+        assert _item['url'] == resource['url']
+        assert _item['id'] == resource['id']
+        assert _item['description'] == resource['description']
+
+    
+    def test_that_json_strings_in_extras_are_parsed_as_json(self):
+
+        params = {
+            'extras' : [
+                {
+                    'key': 'json_extra',
+                    'value': '{\"k1\":\"this is a json\", \"k2\": \"so it should be converted\", \"k3\":\"\\ninto a dict\"}'},
+                {
+                    'key': 'string_extra',
+                    'value':'This is a plain string, so it should stay as it is'
+                }, 
+                {
+                    'key':'none_extra',
+                    'value': None
+                }, 
+                {
+                    'key': 'empty_extra',
+                    'value': ''
+                }
+            ]
         }
-        _resource = helpers.call_action('resource_create', **_params)
-        _resource_view_list = helpers.call_action('resource_view_list', id=_resource['id'])
-        # Check if disabled
-        isEnabled = getLogic.config_disabled(_resource_view_list[0]['id'])
-        data = json.loads(isEnabled)
-        assert (data['catalog'][0]['isEnabled'], False)
 
-        # Enable groups
-        enabled = getLogic.item_enabled(_resource_view_list[0]['id'])
 
-        # Check if disabled
-        isDisabled = getLogic.config_disabled(_resource_view_list[0]['id'])
-        data = json.loads(isDisabled)
-        assert (data['catalog'][0]['isEnabled'], True)
+        dataset = factories.Dataset(**params)
+        resource = factories.Resource(package_id=dataset['id'], format='csv')
+        _model = getLogic._get_model(dataset_id=dataset['id'], resource_id=resource['id'])
 
-    def test_is_config_enabled(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
+        _extras = _model['dataset']['extras']
+        
+        # We expect that extra with key 'json_extra' is parsed into a json (dict)
+        _json_extra = filter(lambda extra: extra['key'] == "json_extra", _extras)[0]
+        assert type(_json_extra['value']) is dict
+        
+        # We expect that extra with key 'string_extra' is parsed into a json (dict)
+        _string_extra = filter(lambda extra: extra['key'] == "string_extra", _extras)[0]
+        if six.PY3:
+            assert type(_string_extra['value']) is str
+        else:
+            assert type(_string_extra['value']) is unicode
+
+
+        _empty_extra = filter(lambda extra: extra['key'] == "empty_extra", _extras)[0]
+        assert len(_empty_extra['value']) == 0
+
+
+    def test_config(self, resource):
+        _resource_view_list = helpers.call_action('resource_view_list', id=resource['id'])
+        _config = json.loads(getLogic._config(_resource_view_list[0]['id']))
+        
+        assert _config
+        assert resource['id'] == _config['catalog'][0]['id']
+
+
+    def test_base_return_item(self, dataset):
         _params = {
-            'package_id': _package['id'],
-            'url': 'http://data',
-            'name': 'A nice resource',
-            'format': 'csv'
-        }
-        _resource = helpers.call_action('resource_create', **_params)
-        _resource_view_list = helpers.call_action('resource_view_list', id=_resource['id'])
-        # Check if disabled
-        isEnabled = getLogic.config_enabled(_resource_view_list[0]['id'])
-        data = json.loads(isEnabled)
-        assert (data['catalog'][0]['isEnabled'], True)
-
-        # disable groups
-
-        disable = getLogic.item_disabled(_resource_view_list[0]['id'])
-
-        # Check if enabled
-        isEnabled = getLogic.config_enabled(_resource_view_list[0]['id'])
-        data = json.loads(isEnabled)
-        assert (data['catalog'][0]['isEnabled'], False)
-
-    def test_config(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _params = {
-            'package_id': _package['id'],
-            'url': 'http://data',
-            'name': 'A nice resource',
-            'format': 'csv'
-        }
-        _resource = helpers.call_action('resource_create', **_params)
-        _resource_view_list = helpers.call_action('resource_view_list', id=_resource['id'])
-        _config = getLogic._config(_resource_view_list[0]['id'])
-        _config = json.loads(_config)
-        assert (_config)
-        assert _resource['id'] == _config['catalog'][0]['id']
-
-    def test_base_return_item(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _params = {
-            'package_id': _package['id'],
+            'package_id': dataset['id'],
             'url': 'http://data',
             'name': 'A nice resource',
             'format': 'csv'
@@ -176,32 +201,30 @@ class TestTerriaLogic(object):
         _base = getLogic._base(_resource_view_list[0]['id'])
         assert _params['url'] == _base['catalog'][0]['url']
 
+
     def test_get_model(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _params = {
-            'package_id': _package['id'],
-            'url': 'http://data',
-            'name': 'A nice resource',
-            'format': 'csv'
-        }
-        _resource = helpers.call_action('resource_create', **_params)
-        _resource_view_list = helpers.call_action('resource_view_list', id=_resource['id'])
-        _get_model = getLogic._get_model(_package['id'], _resource['id'])
-        assert (_package, _get_model['resource'])
-        assert (_resource, _get_model['resource'])
-        assert (self.owner_org, _get_model['organization'])
+
+        _organization = factories.Organization()
+        _dataset = factories.Dataset(owner_org=_organization['id'])
+        _resource = factories.Resource(package_id=_dataset['id'], format="csv")
+
+        _get_model = getLogic._get_model(_dataset['id'], _resource['id'])
+
+
+        # When we add a resource to the dataset, the dataset parameter and _get_model['dataset'] differ for
+        # resources, num_resources, metadata_modified
+        # so we can't compare them altogether and we just compare the ids
+        assert _dataset['id'] == _get_model['dataset']['id']
+
+        assert _resource == _get_model['resource']
+        
+        assert _dataset['owner_org'] == _get_model['organization']['id']
+        
         assert {'base_url': ckan.lib.helpers.url_for('/', _external=True)} == _get_model['ckan']
         assert {'base_url': constants.TERRIAJS_URL} == _get_model['terriajs']
 
-    def test_model(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _params = {
-            'package_id': _package['id'],
-            'url': 'http://data',
-            'name': 'A nice resource',
-            'format': 'csv'
-        }
-        _resource = helpers.call_action('resource_create', **_params)
-        _resource_view_list = helpers.call_action('resource_view_list', id=_resource['id'])
-        _model = getLogic._model(_package['id'], _resource['id'])
-        assert (_model)
+
+    def test_model(self, dataset):
+        _resource = factories.Resource(package_id=dataset['id'], format='csv')
+        _model = getLogic._model(dataset['id'], _resource['id'])
+        assert _model
--- ckanext/terriajs/tests/test_plugin.py
+++ ckanext/terriajs/tests/test_plugin.py
@@ -1,14 +1,8 @@
 '''Tests for plugin.py.'''
 import ckan.plugins
-from ckan.plugins import toolkit
-import ckan.tests.factories as factories
 import ckan.tests.helpers as helpers
 import ckanext.terriajs.constants as constants
-import ckanext.terriajs.tools as tools
-import ckanext.terriajs.logic as getLogic
-from ckan.lib.helpers import get_site_protocol_and_host
-import json
-import requests
+from ckan.plugins import toolkit
 
 ckan_29_or_higher = toolkit.check_ckan_version(u'2.9')
 
@@ -30,89 +24,58 @@ class TestTerria(object):
     @classmethod
     def setup_class(cls):
         helpers.reset_db()
+        
         # Test code should use CKAN's plugins.load() function to load plugins
         # to be tested.
         if not ckan.plugins.plugin_loaded('terriajs'):
             ckan.plugins.load('terriajs')
-        # Create user and context
-        cls.admin = factories.User()
-        cls.context = {
-            'ignore_auth': False,
-            'user': cls.admin['name']
+        
+
+    def test_default_views_are_created_automatically(self, resource):
+
+        _resource_view_list = helpers.call_action('resource_view_list', id=resource['id'])
+        _resource_view_list_formats = [view['terriajs_type'] for view in _resource_view_list]
+
+        # if resource format is one of the DEFAULT FORMATS
+        # check if the plugin created a view for the format
+        if resource['format'].lower() in constants.DEFAULT_FORMATS:
+            assert resource['format'].lower() in _resource_view_list_formats
+
+    def test_can_create_a_terriajs_view(self, terriajs_view):
+        # TODO
+        # remove fixture 
+
+        assert constants.TYPE == terriajs_view['view_type']
+
+
+    def test_can_show_resource_view(self, terriajs_view):
+
+        _view = helpers.call_action('resource_view_show', id=terriajs_view['id'])
+        
+        assert constants.TERRIAJS_CONFIG_KEY in _view
+        assert constants.TERRIAJS_TYPE_KEY in _view
+        assert _view['view_type'] == constants.TYPE
+
+
+    def test_can_update_resource_view(self, terriajs_view):
+
+        # We fetch the view
+        _view = helpers.call_action('resource_view_show', id=terriajs_view['id'])
+
+        params = {
+            'id': _view['id'],
+            'title': 'New title',
+            'description': 'New description'
         }
-        cls.owner_org = factories.Organization(
-            users=[{'name': cls.admin['id'], 'capacity': 'admin'}]
-        )
-        cls.env = {'REMOTE_USER': cls.admin['name'].encode('ascii')}
 
-        cls.package = factories.Dataset(owner_org=cls.owner_org['id'])
+        # We need to pass the whole view dict, not only the values that change
+        # Is this correct?
+        _view.update(params)
 
-        cls.resource = factories.Resource(package_id=cls.package['id'], format='csv')
-
-        # Create view
-        # cls.package = factories.Dataset(owner_org=cls.owner_org['id'])
-        try:
-            resource_type = tools.map_resource_to_terriajs_type(cls.resource)
-        except Exception as e:
-            raise e
-        resource_type = 'csv'
-
-        terriajs_schema = tools.get_schema(resource_type)
-        if not terriajs_schema:
-            raise Exception(resource_type + (' not defined, check your config'))
-
-        terriajs_config = tools.get_config(resource_type)
-
-        cls.params = {
-            'resource_id': cls.resource['id'],
-            'view_type': constants.TYPE,
-            'description': 'A nice view',
-            constants.TERRIAJS_TYPE_KEY: resource_type,
-            constants.TERRIAJS_CONFIG_KEY: terriajs_config
-        }
-    def test_unknown(self):
-        # You pass a resource to the builder
-        # You obtain the parameters
-        # Check into the parameters if the type of the view
-        # Check into the parameters the terriajs type
-        # Do the following tests for this format constants.formats
-        pass
-
-    def test_default_views_are_created_automatically(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _params = {
-            'package_id': _package['id'],
-            'url': 'http://data',
-            'name': 'A nice resource',
-            'format': 'csv'
-        }
-        _resource = helpers.call_action('resource_create', **_params)
-        _resource_view_list = helpers.call_action('resource_view_list', id=_resource['id'])
-        formats = constants.DEFAULT_FORMATS
-
-        # Check if they are views
-        assert (_resource_view_list)
-        # Check if the created view has a format declared in the constants
-        for view in _resource_view_list:
-             if view[constants.TERRIAJS_TYPE_KEY] in formats:
-                 assert True
-                 assert (view['view_type'], constants.TYPE)
-
-
-    def test_can_create_a_terriajs_view(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _resource = factories.Resource(package_id=_package['id'])
-        _resource_view = factories.ResourceView(**self.params)
-        assert constants.TYPE == _resource_view['view_type']
-
-
-    def test_can_show_resource_view(self):
-        _package = factories.Dataset(owner_org=self.owner_org['id'])
-        _resource = factories.Resource(package_id=_package['id'])
-        _resource_view = factories.ResourceView(**self.params)
-
-        result = helpers.call_action('resource_view_show', id=_resource_view['id'])
-        result.pop('id')
-        result.pop('package_id')
-        assert (self.params, result)
+        # We update the view
+        _view = helpers.call_action('resource_view_update', **_view)
+        
+        # assert update had effect
+        assert _view['title'] == params['title']
+        assert _view['description'] == params['description']
 
